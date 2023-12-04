0e16ee0089a6c874221938cf2fe5f6dd7e58bfa9
WEN Hao <wenh06@gmail.com>
2023-12-05 01:01:00 +0800

    update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,28 +1,53 @@
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "4,6,7"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "4,6,7"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2"
 
-import torch
 import datasets
+import einops
+import numpy as np
+import torch
 import transformers
-from transformers import TrainingArguments, Trainer
+from transformers import TrainingArguments, Trainer, EvalPrediction
 
-def top_n_accuracy(preds: torch.Tensor, labels: torch.Tensor, n: int = 1) -> float:
+
+def top_n_accuracy(preds: torch.Tensor, labels: torch.Tensor, n: int = 1) -> torch.Tensor:
+    """
+    Computes the accuracy over the k top predictions for the specified values of k
+    """
     assert preds.shape[0] == labels.shape[0]
     batch_size, n_classes, *extra_dims = preds.shape
+    if isinstance(preds, np.ndarray):
+        preds = torch.from_numpy(preds)
+    if isinstance(labels, np.ndarray):
+        labels = torch.from_numpy(labels)
     _, indices = torch.topk(preds, n, dim=1)  # of shape (batch_size, n) or (batch_size, n, d_1, ..., d_n)
     pattern = " ".join([f"d_{i+1}" for i in range(len(extra_dims))])
     pattern = f"batch_size {pattern} -> batch_size n {pattern}"
     correct = torch.sum(indices == einops.repeat(labels, pattern, n=n))
-    acc = correct.item() / preds.shape[0]
+    acc = correct / preds.shape[0]
     for d in extra_dims:
         acc = acc / d
     return acc
 
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    return top_n_accuracy(logits, labels, n=1)
+
+def compute_metrics(eval_pred: EvalPrediction) -> dict:
+    logits, labels = eval_pred  # numpy.ndarray, numpy.ndarray
+    if isinstance(logits, np.ndarray):
+        logits = torch.from_numpy(logits)
+    if isinstance(labels, np.ndarray):
+        labels = torch.from_numpy(labels)
+    accuracies = {
+        f"top{k}-accuracy": top_n_accuracy(logits, labels, n=k) for k in (1, 3, 5)
+    }
+    return accuracies
+
+
+def freeze_backbone(model):
+    for name, param in model.named_parameters():
+        if "classifier" not in name:
+            param.requires_grad = False
 
 
 def main():
@@ -36,6 +61,8 @@ def main():
 
     model.config.id2label = {i: label for i, label in enumerate(refined_classes)}
     model.num_labels = len(refined_classes)
+
+    freeze_backbone(model)
 
     dataset = datasets.load_dataset(
         "csv",
@@ -74,3 +101,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    # nohup python -u /home/wenhao/Jupyter/wenhao/workspace/AI-Testing/text/data/TextAdvGen/train_model.py > /home/wenhao/Jupyter/wenhao/workspace/AI-Testing/tmp/train.log 2>&1 & echo $! > /home/wenhao/Jupyter/wenhao/workspace/AI-Testing/tmp/train.pid
